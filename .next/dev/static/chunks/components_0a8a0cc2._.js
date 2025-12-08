f81d11000684eda788f1f970e752bfbb84aad658
Phong Nguyen Thanh <phongnt@newimage.local>
2025-12-08 20:09:27 +0700

    fix: error image CPO

--- .next/dev/static/chunks/components_0a8a0cc2._.js
+++ .next/dev/static/chunks/components_0a8a0cc2._.js
@@ -422,7 +422,7 @@ function CPOBuilding() {
                         className: "text-center",
                         children: [
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                                src: "/media/quoc.jpg",
+                                src: "/media/quoc_cpo.png",
                                 alt: "CPO",
                                 width: 300,
                                 height: 300,
